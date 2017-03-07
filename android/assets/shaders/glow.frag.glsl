dab7309f39726097b690aea61eaba2fc84dee6b0
guilherme <guilherme.nemeth@gmail.com>
2017-03-06 21:59:13 -0300

    translate level2

--- android/assets/shaders/glow.frag.glsl
+++ android/assets/shaders/glow.frag.glsl
@@ -16,7 +16,7 @@ void main()
 
 	gl_FragColor = vec4((src + dst) - (src * dst), 1.0);
 */
-        float gamma = 1.15;
+        float gamma = 1.0;
 	vec3 dst = texture2D(u_sampler0, v_texCoord0).rgb;
 	vec3 src = texture2D(u_sampler1, v_texCoord0).rgb;
 	vec3 hdr = (src + dst) - (src * dst);
