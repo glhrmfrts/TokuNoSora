55496afcc3d8eba27dd7800ac0f1b53357bc4b4c
guilherme <guilherme.nemeth@gmail.com>
2017-03-06 21:15:07 -0300

    gamma increase

--- android/assets/shaders/glow.frag.glsl
+++ android/assets/shaders/glow.frag.glsl
@@ -10,8 +10,18 @@ varying vec2 v_texCoord0;
 
 void main()
 {
+/*
 	vec3 dst = texture2D(u_sampler0, v_texCoord0).rgb;
 	vec3 src = texture2D(u_sampler1, v_texCoord0).rgb;
 
 	gl_FragColor = vec4((src + dst) - (src * dst), 1.0);
+*/
+        float gamma = 1.15;
+	vec3 dst = texture2D(u_sampler0, v_texCoord0).rgb;
+	vec3 src = texture2D(u_sampler1, v_texCoord0).rgb;
+	vec3 hdr = (src + dst) - (src * dst);
+	hdr = vec3(1.0) - exp(-hdr * u_exposure);
+	hdr = pow(hdr, vec3(1.0 / gamma));
+
+	gl_FragColor = vec4(hdr, 1.0);
 }
